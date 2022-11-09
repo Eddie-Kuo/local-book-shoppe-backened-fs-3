8142880ed8034409dec87a366ec089844bb19aea
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-09 11:30:58 -0600

    test 3 /authors route written

---
+++ __tests__/authors.test.js
@@ -0,0 +1,24 @@
+const setup = require('../data/setup');
+const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+// request needs to be from supertest and not from express
+
+describe('author routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  test('/authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.body.length).toEqual(10);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      dob: expect.any(Number),
+      pob: expect.any(String),
+    });
+  });
+  afterAll(() => {
+    pool.end;
+  });
+});
